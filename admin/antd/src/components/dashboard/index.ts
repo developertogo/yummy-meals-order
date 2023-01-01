828216c5dc41c0207d16c7cdf3718b4a2b4e8ee7
Carlos Hung <developertogo@gmail.com>
2023-01-01 00:03:15 -0800

    Delete not needed folders

--- admin/antd/src/components/dashboard/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./deliveryMap";
-export * from "./dailyRevenue";
-export * from "./dailyOrders";
-export * from "./newCustomers";
-export * from "./orderTimeline";
-export * from "./trendingMenu";
-export * from "./recentOrders";
--- admin/antd/src/components/product/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./create";
-export * from "./edit";
-export * from "./item";
-export * from "./productCategoryFilter";
